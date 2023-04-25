4a5e957bdf196972ac74f42ed1760dee7fe55b0d
markpkasi <pkasimark6@gmail.com>
2023-04-25 12:45:22 -0700

    sorting algos

--- sort.h
+++ sort.h
@@ -4,13 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /* Comparison direction macros for bitonic sort */
 #define UP 0
 #define DOWN 1
 
 /**
- * enum bool - Enumeration of Boolean values.
+ * enum bool - Enumeration of Boolean values
  * @false: Equals 0.
  * @true: Equals 1.
  */
@@ -23,15 +22,15 @@ typedef enum bool
 /**
  * struct listint_s - Doubly linked list node
  *
- * @n: Integer stored in the node
+ * @n: Integer stored in node
  * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
+ * @next: Pointer to  next element of the list
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* Printing helper functions */
@@ -51,6 +50,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 
-#endif /*SORT_H*/
+#endif /* SORT_H */
